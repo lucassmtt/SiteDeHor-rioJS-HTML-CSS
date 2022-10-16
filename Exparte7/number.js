35ec2530dadbb80e9fac5dcc70c2f52870aeb4ea
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-10-16 09:35:33 -0300

    v2

--- Exparte7/number.js
+++ Exparte7/number.js
@@ -1,1 +1,24 @@
-function 
+let num = document.getElementById('txtnum')
+let lista = document.getElementById('flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1)
+        return true
+    else{return false}
+}
+function add(){
+    if (isNumero(num.value) && !inLista(num.value, valores)){
+        alert('Tudo ok')
+    }
+    else{alert('Número incorreto ou já encontrado.')}
+}
