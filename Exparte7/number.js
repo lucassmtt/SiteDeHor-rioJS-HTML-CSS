ecc4dd9f833a776e6676bddd87be393fc0e6ec85
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-10-17 15:32:34 -0300

    ok

--- Exparte7/number.js
+++ Exparte7/number.js
@@ -1,6 +1,6 @@
 let num = document.getElementById('txtnum')
 let lista = document.getElementById('flista')
-let res = document.querySelector('div#res')
+let res = document.getElementById('res')
 let valores = []
 
 function isNumero(n){
@@ -16,9 +16,43 @@ function inLista(n, l){
         return true
     else{return false}
 }
+
 function add(){
     if (isNumero(num.value) && !inLista(num.value, valores)){
-        alert('Tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else{
+        window.alert('Número incorreto ou já encontrado.')}
+        num.value = ''
+        num.focus()
+}
+
+function ter(){
+    if (valores.length == 0){
+        alert('Adicione dados antes de finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo foram digitados ${valores.length}`
+        res.innerHTML += `<p>O seu maior valor foi ${maior}`
+        res.innerHTML += `<p>O seu menor valor foi ${menor}`
+        res.innerHTML += `<p>A soma dos números digitados dá ${soma}`
+        res.innerHTML += `<p>A média de todos os valores é ${media = soma / valores.length}`
     }
-    else{alert('Número incorreto ou já encontrado.')}
 }
